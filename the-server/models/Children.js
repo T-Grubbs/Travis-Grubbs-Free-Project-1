fcaa8a383be060bd3436927453c48d9368951de2
Travis Grubbs <flygrubbs@yahoo.com>
2019-01-01 23:34:18 -0500

    more changes

--- the-server/models/Children.js
+++ the-server/models/Children.js
@@ -1,20 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const childrenSchema = new Schema({
-
-  first: String,
-  last: String,
-  strikes: Number,
-  
-
-
+	first: String,
+	last: String,
+	strikes: Number,
+	timeouts: []
 });
-
-
-
-
-
 
 module.exports = User;
